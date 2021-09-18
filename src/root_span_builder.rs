51e7911af7d1dd6a9c8265fa19b94224ddff56e2
Aode (lion) <asonix@asonix.dog>
2021-09-18 10:45:32 -0500

    Build error string outside of span.record
    This is a workaround for https://github.com/tokio-rs/tracing/issues/1565

--- src/root_span_builder.rs
+++ src/root_span_builder.rs
@@ -56,11 +56,10 @@ impl RootSpanBuilder for DefaultRootSpanBuilder {
 
 fn handle_error(span: Span, error: &actix_web::Error) {
     let response_error = error.as_response_error();
-    span.record(
-        "exception.message",
-        &tracing::field::display(response_error),
-    );
-    span.record("exception.details", &tracing::field::debug(response_error));
+    let display = format!("{}", response_error);
+    let debug = format!("{:?}", response_error);
+    span.record("exception.message", &tracing::field::display(display));
+    span.record("exception.details", &tracing::field::display(debug));
     let status_code = response_error.status_code();
     span.record("http.status_code", &status_code.as_u16());
 
