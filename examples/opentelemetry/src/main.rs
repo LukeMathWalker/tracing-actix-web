9d2cb0d658e4c2e94069ed118f37f25244b939ed
Omid Rad <omidmr@gmail.com>
2024-07-19 11:29:58 +0200

    Support otel 0.24

--- examples/opentelemetry/src/main.rs
+++ examples/opentelemetry/src/main.rs
@@ -15,10 +15,10 @@ async fn hello() -> &'static str {
 fn init_telemetry() {
     let app_name = "tracing-actix-web-demo";
 
-    // Start a new Jaeger trace pipeline.
+    // Start a new otlp trace pipeline.
     // Spans are exported in batch - recommended setup for a production application.
     global::set_text_map_propagator(TraceContextPropagator::new());
-    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+    let tracer = opentelemetry_otlp::new_agent_pipeline()
         .with_service_name(app_name)
         .install_batch(TokioCurrentThread)
         .expect("Failed to install OpenTelemetry tracer.");
@@ -26,7 +26,7 @@ fn init_telemetry() {
     // Filter based on level - trace, debug, info, warn, error
     // Tunable via `RUST_LOG` env variable
     let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
-    // Create a `tracing` layer using the Jaeger tracer
+    // Create a `tracing` layer using the otlp tracer
     let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
     // Create a `tracing` layer to emit spans as structured logs to stdout
     let formatting_layer = BunyanFormattingLayer::new(app_name.into(), std::io::stdout);
